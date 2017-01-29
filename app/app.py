26002e7f87e217238fe00752fec7d0c19582560c
Rory O'Keeffe <rorokeeffe@gmail.com>
2017-01-29 17:26:04 +0000

    Add a basic CLI and some rudimentary validity checking for phone number and top-up amount

--- app/app.py
+++ app/app.py
@@ -1,13 +1,54 @@
 #!/usr/bin/env python
+import sys
+import re
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-
-import time
 
 from config import info
 
+
+def is_valid_phone_number(number):
+    """Checks phone number is valid"""
+    num_regex = r'08[2-9]\d{7}'
+    if re.match(num_regex, number) is not None:
+        return True
+    return False
+
+
+def is_valid_topup_amount(amount):
+    """Checks the top-up amount conforms to Three's options"""
+    try:
+        amount = float(amount)
+    except:
+        return False
+
+    if ((10.00 <= amount <= 30.00 and amount % 5 == 0) or
+            (30.00 <= amount <= 50.00 and amount % 10 == 0)):
+        return True
+    return False
+
+
+def setup(sysargs):
+    """If arguments are supplied, overrides the config values.
+    Checks that the phone number and top-up amount are valid."""
+    if len(sysargs) == 3:
+        info['number'] = sysargs[1]
+        info['topup_amount'] = sysargs[2]
+
+    if not is_valid_phone_number(info['number']):
+        print('Supplied phone number does not seem like a valid '
+              'Irish phone number...')
+        sys.exit(1)
+
+    if not is_valid_topup_amount(info['topup_amount']):
+        print('Supplied top-up amount doesn\'t match Three\'s options. '
+              'Must be: 10.00, 15.00, 20.00, 25.00, 30.00, 40.00 or 50.00.')
+        sys.exit(1)
+
+
 if __name__ == '__main__':
+    setup(sys.argv)
+
     driver = webdriver.Chrome()
     driver.get('https://mi-pay.three.ie/UnregisteredTopup')
     assert 'Instant Top Up' in driver.title
@@ -68,4 +109,4 @@ if __name__ == '__main__':
     ts_and_cs_box = driver.find_element_by_name('AcceptTermsAndCond')
     ts_and_cs_box.click()
     submit_btn = driver.find_element_by_id('submitButton')
-    submit_btn.click()
+    # submit_btn.click()
